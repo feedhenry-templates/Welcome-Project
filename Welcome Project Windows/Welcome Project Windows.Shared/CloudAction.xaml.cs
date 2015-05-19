2892e30a37cf14ef9c152660506a1c248ab90214
edewit <erikjan.dewit@gmail.com>
2015-05-19 15:40:32 +0200

    initial version of Welcome windows app

---
+++ Welcome Project Windows/Welcome Project Windows.Shared/CloudAction.xaml.cs
@@ -0,0 +1,41 @@
+﻿//using FHSDK;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Welcome_Project_Windows
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CloudAction : Page
+    {
+        public CloudAction()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var message = "Error";
+            //FHResponse res = await FH.Cloud("hello", "GET", null, null);
+            //if (res.StatusCode == System.Net.HttpStatusCode.OK)
+            //{
+            //    message = (string)res.GetResponseAsDictionary()["msg"];
+            //}
+        }
+    }
+}
---
+++ Welcome Project Windows/Welcome Project Windows.Windows/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿//using FHSDK;
+//using FHSDK.Services;
+//using FHSDK81.Phone;
+using System.Diagnostics;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Welcome_Project_Windows
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            Window.Current.SizeChanged += Current_SizeChanged;
+            this.InitializeComponent();
+            this.InitApp();
+        }
+
+        private async void InitApp()
+        {
+            //await FHClient.Init();
+        }
+
+        void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            // Get the new view state
+            // Add: using Windows.UI.ViewManagement;
+            var CurrentViewState = ApplicationView.GetForCurrentView().Orientation;
+            switch (CurrentViewState)
+            {
+                case ApplicationViewOrientation.Landscape:
+                    grid.RowDefinitions.Clear();
+                    grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                    grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+
+                    break;
+
+                case ApplicationViewOrientation.Portrait:
+                    grid.ColumnDefinitions.Clear();
+                    grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(3, GridUnitType.Star) });
+                    grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(7, GridUnitType.Star) });
+
+                    break;
+            }
+        }
+
+        private void Cloud_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Debug.WriteLine("Cloud action clicked!");
+            Frame.Navigate(typeof(CloudAction));
+        }
+
+        private void Grid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Debug.WriteLine("Data browser clicked!");
+        }
+    }
+}
